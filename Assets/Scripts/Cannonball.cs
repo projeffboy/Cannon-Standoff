66b4b3dff9488835e3993b7f8751410c71c27a57
Jeffery <jeffery124@gmail.com>
2020-10-20 22:53:19 +0800

    cannonball bounce

--- Assets/Scripts/Cannonball.cs
+++ Assets/Scripts/Cannonball.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Cannonball : MonoBehaviour {
     public float gravity = -9.81f;
+    public float baseSpeed = 5;
+    public float restitutionCoefficient = 0.75f;
     public float secondsLeft = 10;
 
     // The two variables underneath must be set by another script when the cannon ball is created
@@ -17,7 +19,7 @@ public class Cannonball : MonoBehaviour {
     private float cannonLength = 2.5f;
     private float speedX;
     private float speedY;
-    private bool hittingValley = false;
+    private bool still = false;
 
     private void Start() {
         int direction_X = shootRight ? 1 : -1;
@@ -27,8 +29,7 @@ public class Cannonball : MonoBehaviour {
 
         transform.position += cannonLength / 4 * new Vector3(x_norm, y_norm, 0);
 
-        float initialSpeed = 5f;
-        float magnitude = speedMult * initialSpeed;
+        float magnitude = speedMult * baseSpeed;
 
         speedX = magnitude * x_norm;
         speedY = magnitude * y_norm;
@@ -40,26 +41,32 @@ public class Cannonball : MonoBehaviour {
             gameObject.SetActive(false);
         }
 
-        transform.position += new Vector3(speedX, speedY, 0) * Time.deltaTime;
-        speedY += gravity * Time.deltaTime;
-
-        Debug.DrawRay(transform.position, new Vector3(speedX, speedY, 0), Color.grey);
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, new Vector3(speedX, speedY, 0), 0.1f);
-        if (hit.collider != null && !hittingValley) {
-            hittingValley = true;
-
-            if (hit.collider.gameObject.CompareTag("Valley")) {
-                speedX = 0;
-                speedY = 0;
-            } else if (hit.collider.gameObject.CompareTag("Water")) {
-                gameObject.SetActive(false);
-            }
+        if (Mathf.Pow(speedX, 2) + Mathf.Pow(speedY, 2) < 0.0001) { // 0.01 squared
+            still = true;
         }
 
-        float pos_x = transform.position.x;
-        float pos_y = transform.position.y;
-        if (pos_x < 0 || pos_x > 18 || pos_y < 0 || pos_y > 10) {
-            gameObject.SetActive(false);
+        if (!still) {
+            transform.position += new Vector3(speedX, speedY, 0) * Time.deltaTime;
+            speedY += gravity * Time.deltaTime;
+
+            Debug.DrawRay(transform.position, new Vector3(speedX, speedY, 0), Color.grey);
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, new Vector3(speedX, speedY, 0), 0.1f);
+            if (hit.collider != null) {
+                if (hit.collider.gameObject.CompareTag("Valley")) {
+                    if (hit.normal.x < 0) {
+                        speedX *= -restitutionCoefficient;
+                    }
+                    speedY *= -restitutionCoefficient;
+                } else if (hit.collider.gameObject.CompareTag("Water")) {
+                    gameObject.SetActive(false);
+                }
+            }
+
+            float pos_x = transform.position.x;
+            float pos_y = transform.position.y;
+            if (pos_x < 0 || pos_x > 18 || pos_y < 0 || pos_y > 10) {
+                gameObject.SetActive(false);
+            }
         }
     }
 }
